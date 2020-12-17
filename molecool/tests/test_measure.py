502026bceab22db68a59cbfcd2036b60b5e75818
Noah Wamble <nwamble@nd.edu>
2020-12-17 13:36:40 -0500

    added testing function

--- molecool/tests/test_measure.py
+++ molecool/tests/test_measure.py
@@ -43,3 +43,16 @@ def test_molecular_mass():
     actual_mass = 16.04
 
     assert pytest.approx(actual_mass, abs=1e-2) == calculated_mass
+
+@pytest.mark.parametrize("p1, p2, p3, expected_angle",[
+    (np.array([np.sqrt(2)/2, np.sqrt(2)/2, 0]), np.array([0,0,0]), np.array([1,0,0]), 45),
+    (np.array([0,0,-1]), np.array([0,1,0]), np.array([1,0,0]), 60)
+
+])
+def test_calculate_angle_many(p1,p2,p3,expected_angle):
+    """
+        Calculated
+    """
+    calculated_value= molecool.calculate_angle(p1,p2,p3, degrees=True)
+
+    assert pytest.approx(expected_angle) == calculated_value
